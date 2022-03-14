22e5c4ac995809999de23d7fd9688f695a52aea1
Daniel Hopson <hdhopson88@yahoo.com>
2022-03-14 16:05:16 -0500

    Adds click handler and properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  password: string;
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+  password = '';
 
-  constructor() {
-    this.password = '';
+  constructor() {}
+
+  onChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
   }
 
   submit() {
     this.password = "Huzzah!";
-    console.log(this.password);
+    console.log(this.useLetters);
+    console.log(this.useNumbers);
+    console.log(this.useSymbols);
   }
 }
